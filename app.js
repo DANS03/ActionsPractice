56acf7dd1debf4d1700ba54aaaae0d947c39c39e
DAVID-LAPTOP\david <davidabrahamns@gmail.com>
2023-11-15 07:42:21 -0600

    actions done

---
+++ app.js
@@ -0,0 +1,8 @@
+//const moment = require("moment");
+let i = 5;
+let j = 10;
+console.log("Inicializando...");
+console.log("La suma es " + (i+j));
+
+//let fecha = moment().format("LL");
+//console.log("Y la fecha es: ", fecha);
